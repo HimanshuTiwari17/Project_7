8fa6c8f44fdded52c77bfd903ddfba3637e9f2c5
Himanshu <himanshu.tiwarijmk@gmail.com>
2023-06-20 11:55:21 +0530

    error_handling

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-
-buttonEl=document.getElementById("btn");
-paraEl=document.getElementById("main_joke");
-const apiKey='SRagLKq4UtVI+ZLCDA4gMQ==XR5NjwG0UFpzZZo7';
-const apiUrl="https://api.api-ninjas.com/v1/dadjokes?limit=1";
-async function getJoke(){
-    paraEl.innerText="Processing...";
-    buttonEl.innerText="Loading...";
-    buttonEl.disabled=true;
-    const response = await fetch(apiUrl, {headers: {"X-Api-Key": apiKey,}});
-    const data=await response.json();
-    paraEl.innerText=data[0].joke;
-    buttonEl.innerText="Telll Me a Joke";
-    buttonEl.disabled=false;
+buttonEl = document.getElementById("btn");
+paraEl = document.getElementById("main_joke");
+const apiKey = "SRagLKq4UtVI+ZLCDA4gMQ==XR5NjwG0UFpzZZo7";
+const apiUrl = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+try {
+  async function getJoke() {
+    paraEl.innerText = "Processing...";
+    buttonEl.innerText = "Loading...";
+    buttonEl.disabled = true;
+    const response = await fetch(apiUrl, { headers: { "X-Api-Key": apiKey } });
+    const data = await response.json();
+    paraEl.innerText = data[0].joke;
+    buttonEl.innerText = "Tell Me a Joke";
+    buttonEl.disabled = false;
+  }
+} catch (error) {
+  paraEl.innerText = "An error happened, try again later";
+  buttonEl.disabled = false;
+  buttonEl.innerText = "Tell Me a Joke";
 }
-buttonEl.addEventListener("click",getJoke);
+
+buttonEl.addEventListener("click", getJoke);
